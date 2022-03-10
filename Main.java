c4ce8da0a874e93fe97f6a8d88bce8714e26bf58
AitorTormo <aitorenova1988@gmail.com>
2022-03-10 18:18:12 +0100

    subiendo archivos

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"Juanvi", "Pau", "Laia", "Aitor", "Jorge"};
+
+        for(int i = 0; i < nombres.length; i++){
+            System.out.print(nombres [i] + " ");
+        //for (String nombre : nombres){
+            //System.out.print(nombre + " ");
+
+
+
+        }
+    }
+}
